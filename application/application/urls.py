188d114e1292f3b07772d8e078411f4ff20cf637
Yang Yatuo <gataku_yo@pj100.biz>
2024-09-05 09:12:10 +0900

    👍1-2

--- application/application/urls.py
+++ application/application/urls.py
@@ -2,10 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from application.views.common.health_check import health_check
+from application.views.about_us import about_us  # 追加
 
 router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path("", include(router.urls)),
     path(r"health", health_check, name="health"),
+    path(r"about/", about_us, name="about_us"),  # 追加
 ]
---
+++ application/application/views/about_us.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def about_us(request):
+    return render(request, 'about_us.html')
