cbe81e9aa1446d6a3837a0b002dfc58f0505853e
Yang Yatuo <gataku_yo@pj100.biz>
2024-09-05 14:30:41 +0900

    👍url,docker

--- application/application/urls.py
+++ application/application/urls.py
@@ -1,11 +1,5 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from application.views.common.health_check import health_check
-
-router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path(r"health", health_check, name="health"),
 ]
